dd21d5860e2d3dbc9d4c48b80bce789a1ea17b64
Vikram Ejjagiri <vikramejjagiri08@gmail.com>
2026-06-04 10:49:59 +0200

    Add PDF loader functionality with text extraction

---
+++ pdf_loader.py
@@ -0,0 +1,17 @@
+
+print("LOADER FILE EXECUTED")
+from pypdf import PdfReader
+
+
+def load_pdf(pdf_path):
+    reader = PdfReader(pdf_path)
+
+    text = ""
+
+    for page in reader.pages:
+        page_text = page.extract_text()
+
+        if page_text:
+            text += page_text + "\n"
+
+    return text
